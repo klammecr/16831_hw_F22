e4183625fde4975e71655fa22fe93c1a632b17c6
klammecr <christopher.klammer@gmail.com>
2022-09-06 02:19:51 -0400

    Adding the sample_random_data implementation to the replay buffer

--- hw1/rob831/infrastructure/replay_buffer.py
+++ hw1/rob831/infrastructure/replay_buffer.py
@@ -72,12 +72,17 @@ class ReplayBuffer(object):
                 == self.terminals.shape[0]
         )
 
-        ## TODO return batch_size number of random entries from each of the 5 component arrays above [OK]
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
-
-        return TODO, TODO, TODO, TODO, TODO
+        random_idxs = np.random.permutation(batch_size)
+        return (
+            self.obs[random_idxs],
+            self.acs[random_idxs],
+            self.rews[random_idxs],
+            self.next_obs[random_idxs],
+            self.terminals[random_idxs],
+        )
 
 
     def sample_recent_data(self, batch_size=1):
