7ea40ac523622c44a491b1bb7a99051f124ad03f
Shuli Jiang <11hifish@gmail.com>
2022-10-01 00:40:09 -0400

    update HW2

--- hw2/rob831/policies/MLP_policy.py
+++ hw2/rob831/policies/MLP_policy.py
@@ -87,8 +87,8 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
 
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
-        # TODO: get this from HW1
-        raise NotImplementedError
+        output = self.forward(ptu.from_numpy(observation))
+        return ptu.to_numpy(output)
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -101,9 +101,20 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor):
-        # TODO: get this from HW1
-        raise NotImplementedError
-
+        if self.discrete:
+            logits = self.logits_na(observation)
+            action_distribution = distributions.Categorical(logits=logits)
+            return action_distribution
+        else:
+            batch_mean = self.mean_net(observation)
+            scale_tril = torch.diag(torch.exp(self.logstd))
+            batch_dim = batch_mean.shape[0]
+            batch_scale_tril = scale_tril.repeat(batch_dim, 1, 1)
+            action_distribution = distributions.MultivariateNormal(
+                batch_mean,
+                scale_tril=batch_scale_tril,
+            )
+            return action_distribution
 
 #####################################################
 #####################################################
